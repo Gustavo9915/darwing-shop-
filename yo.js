5fcca944e04e23871e13f6f82daf1251fc1c7969
Gustavo9915 <251703718@prepaenlinea-sep.edu.mx>
2026-04-20 17:39:03 -0600

    estrellas y compartir

---
+++ yo.js
@@ -0,0 +1,24 @@
+function calificar(mun){
+    let estrellas = document.querySelectorAll(".estrellas span");
+    estrellas.forEach((estrella, index) =>{
+        if(index < mun){
+            estrella.style.color = "gold";
+        }else{
+            estrella.style.color = "gray";
+        }
+    });
+    document.getElementById("resultado").innerText =
+        "Calificaste con" + mun + "estrellas"
+}
+function compartir(){
+    let url  = window.location.href;
+    if(navigator.share){
+        navigator.share ({
+            titlle: "mira mi pagina",
+            url: url
+        });
+    }else{
+        navigator.clipboard.writeText(url);
+        alert("link copiado");
+    }
+}
