20a31536d57551de3cc77e60005e959d91d5ce68
Gustavo9915 <251703718@prepaenlinea-sep.edu.mx>
2026-04-22 17:07:25 -0600

    nuevo

--- proceso.js
+++ proceso.js
@@ -1,3 +1,8 @@
  function irArriba(){
        window.scrollTo({top: 0, behavior: `smooth`})
     }
+ function pedido(e){
+    e.preventDefault();
+    document.getElementById ("msg").style.display = "block";
+
+ }
