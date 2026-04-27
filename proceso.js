3b729801b377b19fd1336b9c684a6fb0a8e711ff
Gustavo9915 <251703718@prepaenlinea-sep.edu.mx>
2026-04-27 17:53:43 -0600

    horario

--- proceso.js
+++ proceso.js
@@ -6,3 +6,17 @@
     document.getElementById ("msg").style.display = "block";
 
  }
+ let estado = document.querySelector(".estado-horario");
+ let ahora = new Date();
+ let hora = let.gethours();
+ let dia = ahora.getDay(); //0  = domingo
+ if(dia ===0){
+   estado.innerText = "cerrado(domingo)";
+   estado.style.color = "red";
+ }else if(hora >= 10 && hora < 20){
+   estado.innerText = "disponible ahora";
+   estado.style.color = "#00f00";
+ }else{
+   estado.innerText = "fuera de horario";
+   estado.style.color = "orange";
+ }
