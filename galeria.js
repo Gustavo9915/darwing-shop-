0cc869eab661fcfec3f3a6ef01517bd5e8c8aeae
Gustavo9915 <251703718@prepaenlinea-sep.edu.mx>
2026-04-28 15:56:21 -0600

    mejoras

--- galeria.js
+++ galeria.js
@@ -1,3 +1,17 @@
  function irArriba(){
        window.scrollTo({top: 0, behavior: `smooth`})
     }
+const btn = document.getElementById("btnModo");
+btn.addEventListener("click", () =>{
+      document.body.classList.toggle("oscuro");
+      let activo = document.body.classList.contains("oscuro");
+      localStorage.setItem("modoOscuro", activo);
+      btn.innerText = activo ? "☀️" : "🌙"
+});
+window.addEventListener("load", () =>{
+      let guardado = localStorage.getItem("modoOscuro");
+      if(guardado === "true"){
+            document.body.classList.add("modoOscuro");
+            btn.innerHTML = "☀️";
+      }
+});
